293486bdbe016635d77962f71e952ea313ad2625
lsy3709 <lsy3709@naver.com>
2026-03-06 16:24:26 +0900

    7교시, RestController , 댓글 작업, 댓글 쓰기 작업 4, ReplyRepository 작업,댓글 조회, 단위테스트

--- src/main/java/com/busanit501/springboot0226/domain/Reply.java
+++ src/main/java/com/busanit501/springboot0226/domain/Reply.java
@@ -10,6 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString(exclude = "board") // 댓글 테이블 내용만 조회를 하겠다. 부모 게시글는 조회 안하겠다.
+//name = "idx_reply_board_bno" : 게시글 번호로 만든 인덱스라는 의미, ex) idx_테이블명_컬럼명
+// columnList = "board_bno" : 어떤 컬럼을 기준으로 목록을 만들것인가? 결정.
+// board_bno 이름은 클래스 이름이 아니라, 데이터베이스의 컬럼명이 들어간다.
+@Table(name = "Reply", indexes = {
+        @Index(name = "idx_reply_board_bno", columnList = "board_bno")
+})
+
 public class Reply extends BaseEntity{
 
     @Id
